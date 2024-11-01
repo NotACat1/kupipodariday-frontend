2e3d433c2f43dfcb061651b244e251a92a6a6f0d
NotACat1 <notacat.mail@gmail.com>
2024-11-01 13:07:00 +0300

    feat: вынесение URL в ENV файл

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://167.235.140.175:3001";
+export const URL = process.env.REACT_APP_API_URL;
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
