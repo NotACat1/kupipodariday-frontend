6684d3beab0a8d3eabe5013fc058cefc5ac9559a
NotACat1 <notacat.mail@gmail.com>
2024-11-02 18:41:22 +0300

    feat: docker

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = process.env.REACT_APP_API_URL;
+export const URL = `${process.env.REACT_APP_BACKEND_HOST}:${process.env.REACT_APP_BACKEND_PORT}`;
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
